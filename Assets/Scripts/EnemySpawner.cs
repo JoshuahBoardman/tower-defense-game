2bdeb6040c3e1f54b7124273a708316071c693b8
JoshuahBoardman <joshuahboardman@gmail.com>
2020-08-17 17:55:46 -0400

    The enemy spawnin script can support spawning

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour 
+{
+	[SerializeField] float secondsBetweenSpawns;
+	[SerializeField] EnemyMovement enemy;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(SpawnEnimies());
+	}
+	
+IEnumerator SpawnEnimies()
+	{ 
+		while (true)// forever
+		{
+			print("Spawning!");
+			//Instantiate(enemy, enemy.transform, enemyRotaion);
+			yield return new WaitForSeconds(secondsBetweenSpawns);
+		}
+    }
+}
